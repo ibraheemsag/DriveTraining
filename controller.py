38fece39267ce66040fb9c8f9e8da477ed74e926
Ibraheem Alsaghier <ibraheemalsaghier@ibraheems-mbp.kaust.edu.sa>
2025-04-24 20:22:08 +0300

    Made first results

--- controller.py
+++ controller.py
@@ -1,18 +1,23 @@
 import pystk
+import numpy as np
 
 
 def control(aim_point, current_vel, steer_gain=6, skid_thresh=0.2, target_vel=25):
-    import numpy as np
     #this seems to initialize an object
     action = pystk.Action()
+    action.steer = aim_point[0]/3
 
-   
- 
+    if current_vel < target_vel:
+        action.acceleration = 1
+    else:
+        action.acceleration = 0
+        action.brake = True
+    if aim_point[0] < -0.8:
+        action.drift = True
+    if aim_point[0] > 0.8:
+        action.nitro = True
     
 
-    #compute acceleration
-    action.acceleration = 0.1
-    
 
         
 
